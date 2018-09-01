4a1971934b8d80b4ed5dfb2e342acf9ac2de75f5
I-ninja <jtime95@gmail.com>
2018-09-01 19:25:18 -0400

    Finished UI intergration

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,31 +6,45 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 	public const int maxHealth = 100;
 	public int currentHealth = maxHealth;
-	public int bullets = 5
-	bool isDead
-	bool hurt
+	public int bullets = 5;
+	bool isDead;
+	bool hurt;
+	public Text printHealth;
+	public Text printBullets;
 	
+	void Awake (){
+		
+	}
 	void Update ()
 	{
+		printBullets.text = "" + bullets;
+		printHealth.text = "" + currentHealth;
 		if(hurt)
 		{
 		 
 		}
 	}
 	
-	public void  onHit(int amount)
-	{
+	//take out later
+	public void onFir(int amount) {
+		bullets -=amount;
+		if(bullets <=0){
+			Debug.Log("Reload");
+			bullets = 5;
+		}
+	}
+	
+	public void onHit(int amount){
 		currentHealth -= amount;
-		if(currentHealth <= 0 && !isDead)
+		if(currentHealth <= 0)
 		{
 			currentHealth = 0;
-			
 			//change on creation of death state
 			Death();
 		}
 	}
-	public void onPickup int amount){
-		currentHealth += ammount;
+	public void onPickup(int amount){
+		currentHealth += amount;
 		if(currentHealth >= maxHealth)
 		{
 			currentHealth = maxHealth;
