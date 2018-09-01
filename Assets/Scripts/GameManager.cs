620fe4a7b23ac92fee386b88b6f8edb11e78e573
Dominic Brown <domingomonk@gmail.com>
2018-09-01 16:07:59 -0400

    Made player able to shoot via raycast and report to GameManager. Currently it prints out the name of the object shot

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        // Singleton
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void PlayerShotObject(RaycastHit hitInfo)
+    {
+        Debug.Log(hitInfo.collider.name);
+    }
 }
