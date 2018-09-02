d2cf58ca02bb14340558a8b99430de289b729dbe
Dalton Teague <dnteague@ncsu.edu>
2018-09-02 01:05:55 -0400

    adding camera flash when damage taken

--- Assets/camMoveScript.cs
+++ Assets/camMoveScript.cs
@@ -7,21 +7,41 @@ public class camMoveScript : MonoBehaviour {
 	// Transforms to act as start and end markers for the journey.
         private Transform nextPoint;
 
+        public GameObject bloodImage;
+
         // Movement speed in units/sec.
         public float speed = 5.0F;
         public float rotateSpeed = 0.1F;
 
+        private bool getDamage;
+        private Color color;
 
         void Start()
         {
             nextPoint = GameObject.Find("Checkpoint 1").transform;
             transform.LookAt(nextPoint.position);
-            
+            color = bloodImage.GetComponent<Renderer>().material.color;
         }
         
         public void SetNextPoint(Transform checkpoint) {
             Debug.Log("Cam: Setting next point to " + checkpoint.name);
             nextPoint = checkpoint;
+        }
+
+        public void RedFlash() {
+
+            getDamage = true;
+
+            StartCoroutine(Wait());
+
+        }
+
+        public IEnumerator Wait() {
+
+            yield return new WaitForSeconds(2);
+    
+            getDamage = false;
+
         }
 
         // Follows the target position like with a spring
@@ -44,6 +64,16 @@ public class camMoveScript : MonoBehaviour {
             
             }
         
-           
+           if (getDamage)
+         {
+            bloodImage.SetActive(true);
+            
+            color.a -= 20f;
+            bloodImage.GetComponent<Renderer>().material.color -= new Color(0,0,0,.10f);
+         }
+         if (!getDamage)
+         {
+            //bloodImage.SetActive(false);
+         }
         }
 }
