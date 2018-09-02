36c14c92117e91257da41d1f63a2246c56263ba2
Dalton Teague <dnteague@ncsu.edu>
2018-09-02 13:21:40 -0400

    added monster sprites

---
+++ Assets/VideoScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoScript : MonoBehaviour {
+
+    VideoPlayer videoPlayer;
+
+	private void Start()
+{
+    videoPlayer.prepareCompleted += VideoPlayer_prepareCompleted;
+    videoPlayer.Prepare();
+}
+
+private void VideoPlayer_prepareCompleted(VideoPlayer source)
+{
+    videoPlayer.Play();
+} 
+}
